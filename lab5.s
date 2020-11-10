91502af319ad13bb200b5f864ad4883dc527889f
Tachun Lin <djlin@bradley.edu>
2020-11-10 08:31:50 -0600

    remove the solution from lab5.s

--- lab5.s
+++ lab5.s
@@ -40,24 +40,7 @@ main:
 
     # [ implemeent your code here ]
 
-    # prompt to enter the second integer
-    li $v0, 4
-    la $a0, prompt2
-    syscall
-
-    # read the second integer and store it in $t1
-    li $v0, 5
-    syscall
-    move $t1, $v0
-
-		# prepare the parameters for the function call
-		move $a0, $t0
-		move $a1, $t0
-		move $a2, $t1
-
     # call the power function
-    # function call will carry the parameters through
-    # $a0, $a1, $a2, $a3
     jal power
 
 # Terminate the program
@@ -68,29 +51,6 @@ main:
 
 power:
     # [ implement the power function here ]
-    # use $a0 for multiplication result
-    # I fix the $a1 for the base value
-		mul $a0, $a0, $a1
-		
-		# print out the temp values
-    li $v0, 1
-		syscall
-
-		# to avoid $a0 from being overwritten...
-		move $t0, $a0
-
-		# syscal to print new line char to separate the output
-		li $v0, 4
-		la $a0, newline
-		syscall
-		
-		# restore the $a0 value for following calc.
-		move $a0, $t0
-
-		# check if we should continue the loop
-		# we have the original power term stored in $a2
-		sub $a2, $a2, 1
-		bne $a2, 0, power
 
     # return to the function caller
     jr $ra
